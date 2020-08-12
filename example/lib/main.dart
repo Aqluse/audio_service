5d9853fdf096a7ad0df53f2f8765759e8a206faa
Aqluse <Aqluse@users.noreply.github.com>
2020-08-12 16:38:30 +0500

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -313,3 +313,33 @@ class MediaLibrary {
 
   List<MediaItem> get items => _items;
 }
+
+class Seeker {
+  final AudioPlayer player;
+  final Duration positionInterval;
+  final Duration stepInterval;
+  final MediaItem mediaItem;
+  bool _running = false;
+
+  Seeker(
+    this.player,
+    this.positionInterval,
+    this.stepInterval,
+    this.mediaItem,
+  );
+
+  start() async {
+    _running = true;
+    while (_running) {
+      Duration newPosition = player.position + positionInterval;
+      if (newPosition < Duration.zero) newPosition = Duration.zero;
+      if (newPosition > mediaItem.duration) newPosition = mediaItem.duration;
+      player.seek(newPosition);
+      await Future.delayed(stepInterval);
+    }
+  }
+
+  stop() {
+    _running = false;
+  }
+}
